3251f8bbf20fcc37d27611314f1c135b67c3c0b9
Alexandr <sparkygino@gmail.com>
2025-11-03 13:03:33 +0300

    Remove proxy and turnstile token

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const TURNSTILE_SECRET = process.env.TURNSTILE_SECRET;
+// const TURNSTILE_SECRET = process.env.TURNSTILE_SECRET;
 const BITRIX_WEBHOOK = process.env.BITRIX_WEBHOOK;
 const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN || "onkron.us";
 
 // --- Настройка Express ---
-app.set("trust proxy", true);
+// app.set("trust proxy", true);
 app.use(helmet());
 app.use(express.json());
 
